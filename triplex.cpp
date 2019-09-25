d6e70bddf629df73c43b207feda574b2dd7fe5e6
Steven Aragon <stevearagon92@gmail.com>
2019-09-24 21:22:43 -0300

    variables

--- triplex.cpp
+++ triplex.cpp
@@ -5,5 +5,16 @@ int main(){
     std::cout << std::endl;
     std::cout << "You need to enter the correct codes to continue...";
 
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
