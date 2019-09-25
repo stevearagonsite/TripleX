88f08ae39890701bf00ff129e00f04fb511b2666
Steven Aragon <stevearagon92@gmail.com>
2019-09-24 21:48:31 -0300

    Statements and comments

--- triplex.cpp
+++ triplex.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 int main(){
+    // Print welcome messages to the terminal
     std::cout << "You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout << "You need to enter the correct codes to continue...";
 
+    // Declare 3 number code
     const int a = 4;
     const int b = 3;
     const int c = 2;
@@ -12,6 +14,7 @@ int main(){
     const int sum = a + b + c;
     const int product = a * b * c;
 
+    // Print sum and product to the terminal
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
